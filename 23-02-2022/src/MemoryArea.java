8afe0d15a15bc4a56f59666bed0bf67bd6d87d91
Pratik Dandare <pgdandare@gmail.com>
2022-02-23 20:30:14 +0530

    memory area and some FAQ

---
+++ 23-02-2022/src/MemoryArea.java
@@ -0,0 +1,22 @@
+
+public class MemoryArea {
+	
+	public static void main(String[] args) {
+		
+//		new Main();// object or annomus object
+		//heap memory area stores the object
+		
+		MemoryArea main = new MemoryArea();
+		
+		//Main main ---> (variables are store in stack)this part is going to store in stack memory
+	
+		
+		// static variable or methods store in class area
+		
+		//PC(program counter) ----> track current line execution
+		
+		
+		//native ----->  to call other langauge methos we use native memory area
+	}
+
+}
---
+++ 23-02-2022/src/SaveJavaProgram.java
@@ -0,0 +1,11 @@
+
+public class SaveJavaProgram {
+	
+	//TODO window procedure wondows -> show view -> task
+
+//TODO	is it possibule to save java program using differnt name ??
+//	is it possibule to save java program using differnt name --- cmd
+//	is it possibule to have multiple public classes
+//	is it nessesary to store same class name have public keyword
+	
+}
